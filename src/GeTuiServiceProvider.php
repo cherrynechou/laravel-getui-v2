73a72166d6b9d7de97ac0914a482b55525ddb9f4
cherrynechou <iboy_2008@163.com>
2021-06-24 15:05:17 +0800

    first commit

---
+++ src/GeTuiServiceProvider.php
@@ -0,0 +1,29 @@
+<?php
+namespace CherryneChou\GeTui;
+
+use Illuminate\Support\ServiceProvider;
+
+class GeTuiServiceProvider extends ServiceProvider
+{
+      /**
+     * Bootstrap any application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+
+    }
+
+    /**
+     * Register any application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+
+    }
+
+
+}
