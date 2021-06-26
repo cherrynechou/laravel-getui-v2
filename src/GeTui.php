8ba834bbd4d55e73877cee7e4b661184600571e9
cherrynechou <iboy_2008@163.com>
2021-06-26 19:12:25 +0800

    wip

--- src/GeTui.php
+++ src/GeTui.php
@@ -1,20 +1,44 @@
 <?php
 namespace CherryneChou\GeTui;
 use Illuminate\Support\Facades\Cache;
+use Ramsey\Uuid\Uuid;
 
 require_once dirname(__FILE__) . '/getui/GTClient.php';
 require_once dirname(__FILE__) . '/getui/request/push/GTPushMessage.php';
 require_once dirname(__FILE__) . '/getui/request/push/GTNotification.php';
 
-class Getui implements PushInterface
+class GeTui implements PushInterface
 {
+    /**
+     * @var array|null
+     */
     protected $config;
 
+    /**
+     * @var mixed
+     */
     protected $gt_appid;
+
+    /**
+     * @var mixed
+     */
     protected $gt_appkey;
+
+    /**
+     * @var mixed
+     */
     protected $gt_appsecret;
+
+    /**
+     * @var mixed
+     */
     protected $gt_mastersecret;
 
+    /**
+     * GeTui constructor.
+     * @param array|null $config
+     * @throws \Exception
+     */
     public function __construct(array $config = null)
     {
           $this->config = $config;
@@ -37,7 +61,7 @@ class Getui implements PushInterface
             $object = unserialize($sObject);
         }else{
             $object = new GTClient($this->config['gt_domainurl'],  $this->gt_appkey,  $this->gt_mastersecret);
-            $sObject = serialize($user);
+            $sObject = serialize($object);
             Cache::rememberForever('getuiClass', $sObject);
         }
 
@@ -46,6 +70,9 @@ class Getui implements PushInterface
 
     public function pushToOne($deviceId, array $data, $isNotice = true)
     {
+        if (empty($deviceId)) {
+            throw new \Exception('device_id not empty');
+        }
 
         if (!isset($data['content']) || !isset($data['title'])) {
           throw new \Exception('content and title not empty');
@@ -53,10 +80,10 @@ class Getui implements PushInterface
 
         $client = $this->getInstance();
         //设置推送参数
-        $push = new GTPushRequest();
-        $push->setRequestId($deviceId);
-        $message = new GTPushMessage();
-        $notify = new GTNotification();
+        $push = new \GTPushRequest();
+        $push->setRequestId(strtoupper(Uuid::uuid4()->toString()));
+        $message = new \GTPushMessage();
+        $notify = new \GTNotification();
         $notify->setTitle($data['title']);
         $notify->setBody($data['content']);
 
@@ -65,15 +92,19 @@ class Getui implements PushInterface
         $notify->setClickType("none");
         $message->setNotification($notify);
         $push->setPushMessage($message);
-        $push->setCid("CID");
+        $push->setCid($deviceId);
         //处理返回结果
         return $api->pushApi()->pushToSingleByCid($push);
-
     }
 
     public function batchPush($deviceId, array $data)
     {
 
+    }
+
+    public function clearObject()
+    {
+        Cache::forget('getuiClass');
     }
 
 }
